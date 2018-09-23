19bfa8a301838c14a8558da3eb265ced6515cfc8
yogisen <36984500+yogisen@users.noreply.github.com>
2018-09-23 18:45:51 +0200

    Delete query.py

--- query.py
+++
@@ -1,8 +0,0 @@
-# utuiliser Python avec SQLite
-
-import sqlite3
-connexion = sqlite3.connect("factbook.db")
-
-c=connexion.cursor()
-c.execute('select name, population from facts where population != "" order by population asc limit 10;')
-print(c.fetchall())
